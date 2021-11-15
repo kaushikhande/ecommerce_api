1c1c3efa53daca93a8f9cd118cb3c52b14c34f08
kaushikhande <kaushik.hande.vc@startv.com>
2021-11-15 16:31:57 +0530

    added product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..5).each do |index|
+  Product.find_or_create_by(name: "Product #{index}", price: index * 5)
+end
